b0418862c12a802d45c15453e6284a5cfa6b0f2a
Kwibuka Elie <chenqiua@gmail.com>
2022-11-06 01:46:41 +0200

    Birthday Reminder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data)
+  return <>
+  <section className="container">
+  <h3>{people.length} Birthdays today</h3>
+  <List people={people}/>
+  <button onClick={()=> setPeople([])}>Clear all</button>
+  </section>
+  </>;
+}
+
+export default App;
